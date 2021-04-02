04e79cd7b3de4d798b0be45967769709ef88fd2d
Everton <everton.aaraujo@outlook.com>
2021-04-02 11:48:03 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Main from './src/pages/Main';
+
+const App = () => {
+  return (
+    <Main>
+
+    </Main>
+  );
+}
+
+export default App;
